f09d880af0e20635657ead647bd9e50ffe686a3f
topheryun <46732742+topheryun@users.noreply.github.com>
2020-10-12 14:49:56 -0700

    alpha

--- mh-map-helper.js
+++ mh-map-helper.js
@@ -4,17 +4,17 @@ window.onload=function() {
 }
 
 function createResults() {
-var list = document.getElementById("result");
-list.innerHTML = "<tr></tr>";
-var textArea = document.getElementById("input");
-var lines = textArea.value.split("\n");
-var value;
+    var list = document.getElementById("result");
+    list.innerHTML = "<tr></tr>";
+    var textArea = document.getElementById("input");
+    var lines = textArea.value.split("\n");
+    var value;
 
-for (var i = 0; i < lines.length; i++) {
-    if (lines[i] != "") {
-        value = miceValues.get(lines[i]);
-        list.innerHTML += `<tr><td>${lines[i]}</td><td>${value}</td></tr>`;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i] != "") {
+            value = miceValues.get(lines[i]);
+            list.innerHTML += `<tr><td>${lines[i]}</td><td>${value}</td></tr>`;
+        }
+
     }
-
-}
 }
