250f752a8cc56276d37e792a1fef33c61fd4463e
topheryun <46732742+topheryun@users.noreply.github.com>
2020-10-13 14:54:56 -0700

    finished middle row second column for custom input

--- mh-map-helper.js
+++ mh-map-helper.js
@@ -2,17 +2,53 @@
 var snipeTotalCost = 0;
 var sbEarned = 0;
 var profit = 0;
+var leechSpots = 4;
+var leechPrice = 0;
+var dustCost = 210;
 
 window.onload=function() {
-		// when user presses "enter" in text area, it calls createResults()
-    document.getElementById("input")
-		.addEventListener("keyup", function(event) {
-			event.preventDefault();
-			if (event.keyCode === 13) {
-				createResults();
-			}
-		});
+	
+	// when user presses "enter" in text area, it calls createResults()
+	document.getElementById("input")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			createResults();
+		}
+	});
+	
+	document.getElementById("leech-price")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			leechPriceChange();
+		}
+	});
+	
+	document.getElementById("leech-spots")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			leechSpotsChange();
+		}
+	});
 		
+	document.getElementById("snipe-cost")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			snipeCostChange();
+		}
+	});
+	
+	document.getElementById("dust-cost")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			dustCostChange();
+		}
+	});
+	
 }
 
 function createResults() {
@@ -33,59 +69,62 @@ function createResults() {
 
     }
 		snipeTotalCostOutput.value = snipeTotalCost;
-		calcPrices();
+		calcPrices(0);
 		
 		// console.log(snipeTotalCost);
 }
 
 function isRareChange() {
 	var isRare = document.getElementById("is-rare").checked;
-	// var other1 = document.getElementById("map-diff").selectedIndex;
-	// var other2 = document.getElementById("map-type").selectedIndex;
+	//var other1 = document.getElementById("map-diff").selectedIndex;
+	//var other2 = document.getElementById("map-type").selectedIndex;
 	var dustCostOutput = document.getElementById("dust-cost");
 	
-	if (isRare) dustCostOutput.value = 210;
-	else dustCostOutput.value = 0;
+	if (isRare) dustCost = 210;
+	else dustCost = 0;
+	
+	dustCostOutput.value = dustCost;
 	//if (other1 != 0 && other2 != 0) calcPrices();
-	calcPrices();
+	calcPrices(0);
 }
 
 function mapDiffChange() {
 	//var e = document.getElementById("map-diff");
 	//var strUser = e.options[e.selectedIndex].text;
 	var other = document.getElementById("map-type").selectedIndex;
-	if (other != 0) calcPrices();
+	if (other != 0) calcPrices(0);
 
 	//var changeTest = document.getElementById("test");
 	//changeTest.innerHTML = strUser;
 }
 
 function mapTypeChange() {
-	//var e = document.getElementById("map-type");
+	var e = document.getElementById("map-type");
 	//var strUser = e.options[e.selectedIndex].text;
 	var other = document.getElementById("map-diff").selectedIndex;
 	
-	if (other != 0 || e.selectedIndex == 4) calcPrices();
+	if (other != 0 || e.selectedIndex == 4) calcPrices(0);
 	
 	//var changeTest = document.getElementById("test");
 	//changeTest.innerHTML = strUser;
 }
 
-function calcPrices() {
-	var leechPriceOutput = document.getElementById("leech-price");
-	var leechSpotsOutput = document.getElementById("leech-spots");
-	var snipeTotalCostOutput = document.getElementById("snipe-cost");
-	var dustCostOutput = document.getElementById("dust-cost");
-	var profitOutput = document.getElementById("profit");
+function calcPrices(lp) {
 	var isRare = document.getElementById("is-rare").checked;
+
+	if (lp == 0) {
+		leechPrice = 0;
+		if (!isRare) leechPrice -= 40;
+		calcLeechPrice();
+	}
+
+	calcProfit();
+}
+
+function calcLeechPrice() {
+	var leechPriceOutput = document.getElementById("leech-price");
 	var diff = document.getElementById("map-diff").selectedIndex;
 	var type = document.getElementById("map-type").selectedIndex;
-	var leechSpots = leechSpotsOutput.value;
-	var leechPrice = 0;
-	if (!isRare) leechPrice = -40;
-	sbEarned = 0;
-	
-
 	
 	if (diff == 0 || type == 0) { // default
 		leechPrice = 0;
@@ -128,8 +167,43 @@ function calcPrices() {
 		if (isRare) leechPriceOutput.value = leechPrice; 
 		else leechPriceOutput.value = 0;
 	}
-	
+}
+
+function calcProfit() {
+	var profitOutput = document.getElementById("profit");
 	sbEarned = leechSpots * leechPrice;
-	profit = sbEarned - snipeTotalCostOutput.value - dustCostOutput.value;
+	
+	console.log("dustCost: " + dustCost);
+	
+	profit = sbEarned - snipeTotalCost - dustCost;
 	profitOutput.innerHTML = `Profit: ${profit}`;
 }
+
+function leechPriceChange() {
+	var leechPriceOutput = document.getElementById("leech-price");
+	leechPrice = leechPriceOutput.value;
+	
+	calcPrices(leechPrice);
+}
+
+function leechSpotsChange() {
+	var leechSpotsOutput = document.getElementById("leech-spots");
+	leechSpots = leechSpotsOutput.value;
+	
+	calcPrices(leechPrice);
+}
+
+function snipeCostChange() {
+	var snipeCostOutput = document.getElementById("snipe-cost");
+	snipeTotalCost = snipeCostOutput.value;
+	
+	calcPrices(leechPrice);
+}
+
+function dustCostChange() {
+	var dustCostOutput = document.getElementById("dust-cost");
+	dustCost = dustCostOutput.value;
+	
+	calcPrices(leechPrice);
+}
+
